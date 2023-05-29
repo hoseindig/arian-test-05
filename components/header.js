88a52ac7914bf70422acc07cb795f87665738803
h.sheikhi <@h.sheikhi>
2023-05-29 12:06:58 +0330

    header action fix

--- components/header.js
+++ components/header.js
@@ -1,5 +1,18 @@
 import Link from "next/link";
+import { useSelector, useDispatch } from "react-redux";
+import { authAction } from "../store/auth";
+import { useRouter } from "next/router";
+
 const Header = () => {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  console.log(auth);
+  const logOut = () => {
+    dispatch(authAction.logout());
+    router.push("/");
+  };
   return (
     <>
       <div className="header-layout">
@@ -12,9 +25,24 @@ const Header = () => {
           <span>about</span>{" "}
         </Link>
         |{" "}
-        <Link href="login">
-          <span>login</span>{" "}
-        </Link>
+        {!auth.isAuth && (
+          <Link href="login">
+            {!auth.isAuth && <span>login</span>}{" "}
+            {auth.isAuth && (
+              <span onClick={logOut}>logout ( {auth.username} ) </span>
+            )}{" "}
+          </Link>
+        )}
+        {auth.isAuth && (
+          <Link href="product">
+            <span>product</span>{" "}
+          </Link>
+        )}
+        {auth.isAuth && (
+          <p className="logout" onClick={logOut}>
+            logout ( {auth.username} ){" "}
+          </p>
+        )}
       </div>
     </>
   );
